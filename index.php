52c0900240f9720ab040e31b8e22ce34c002448e
eya2018 <eyazaibi@gmail.com>
2018-04-13 12:10:32 +0200

    centrer le tiltre

--- index.php
+++ index.php
@@ -3,7 +3,19 @@
  <head>
  <meta charset="utf-8" />
  <title>Mon site DEMO</title>
+ <style type="text/css"> 
+ h1{
+	 position:absolute;
+	 top:40%;
+	 width:40%;
+	 text-align:center;
+	 font-family:Helvetica;
+	 font-size:100;
+	 text-align:center;
+ }
+ </style>
  </head>
+ 
  <body>
  <h1>FIRST TITLE</h1>
 
