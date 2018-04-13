8d50aba0c96994da8f5e5eef63450213e5e4ba5e
eya2018 <eyazaibi@gmail.com>
2018-04-13 11:37:34 +0200

    mon deuxième changement sur index

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <meta charset="utf-8" />
+ <title>Mon site DEMO</title>
+ </head>
+ <body>
+ <h1>FIRST TITLE</h1>
+
 <?php
 
-echo "hello world";
+echo "<br/><br/>hello world";
 
 ?>
+</body>
+</html>
